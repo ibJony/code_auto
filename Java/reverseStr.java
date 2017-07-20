9ed9963c74e25d2855008067ecbb14f46745ea3b
ibJony <joniezpal@gmail.com>
2017-07-20 20:00:00 +0100

    java

--- Java/reverseStr.java
+++ Java/reverseStr.java
@@ -1,5 +1,13 @@
 
-
+static String recursiveMethod(String str)
+{
+     if ((null == str) || (str.length() <= 1))
+     {
+            return str;
+     }
+ 
+     return recursiveMethod(str.substring(1)) + str.charAt(0);
+}
 
 public class ReverseTheString
 {
