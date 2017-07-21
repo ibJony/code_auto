f889f63482d05ccc9c94ce1a6e70a03582b1d1e4
ibJony <joniezpal@gmail.com>
2017-07-21 20:00:00 +0100

    java

--- Java/reverseStr.java
+++ Java/reverseStr.java
@@ -1,4 +1,12 @@
 
+String str = "MyJava";
+ 
+char[] strArray = str.toCharArray();
+ 
+for (int i = strArray.length - 1; i >= 0; i--)
+{
+    System.out.print(strArray[i]);     //Output : avaJyM
+
 static String recursiveMethod(String str)
 {
      if ((null == str) || (str.length() <= 1))
